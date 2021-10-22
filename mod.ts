3ec2b183ef3b40fe3089a8a04ddd3987dfa8c56d
Dominic Vonk <info@dovocode.com>
2021-10-22 21:27:27 +0200

    Update

--- mod.ts
+++ mod.ts
@@ -1,4 +1,16 @@
 addEventListener("fetch", (event) => {
-  console.log(event.request.body);
+  const request = event.request;
+  const contentType = request.headers.get("content-type");
+  const responseInit = {
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+    },
+  };
+
+  // Handle JSON data.
+  if (contentType.includes("application/json")) {
+    const json = await request.json();
+    return new Response(JSON.stringify({ json }, null, 2), responseInit);
+  }
   event.respondWith(new Response("Hello world"));
 });
